045b87c0e345e3cb551d4771d7eac643b6cbe5a9
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-30 00:23:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,16 @@
 lass Application
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path=="/items/#{item.name}"
       resp.write "#{item.price}"
     else
       resp.write "Route not found"
       resp.status = 404
     end
- 
+
     resp.finish
   end
 end
